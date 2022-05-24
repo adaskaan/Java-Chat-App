8b3bbf59fa74e4db347d329a18602a20f43907bb
Ömer Kaan Adaş <adaskaan@gmail.com>
2022-05-24 11:44:50 +0300

    simplified

---
+++ src/newpackage/Client.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author skaya
+ */
+public class Client {
+
+    static Socket clientSocket;
+    static String serverIp = "localhost";//"localhost";//127.0.0.1
+    static int serverPort = 5000;
+    static ObjectOutputStream clientOutput;
+    static ObjectInputStream clientInput;
+
+    public static void main(String[] args) {
+
+        try {
+            clientSocket = new Socket(serverIp, serverPort);
+            clientInput = new ObjectInputStream(clientSocket.getInputStream());
+            clientOutput = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            clientOutput.writeObject("merhaba server");
+            clientOutput.writeObject("nasılsın server");
+          
+            String value = clientInput.readObject().toString();//blocking
+            System.out.println("Server said:" + value);
+
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ src/newpackage/Server.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author skaya
+ */
+public class Server extends Thread {
+
+    public ServerSocket serverSocket;
+    public int serverPort = 5000;
+    public Thread listen;
+    public ObjectOutputStream clientOutput;
+    public ObjectInputStream clientInput;
+    public int clientId;
+    public ArrayList<SClient> clients;
+
+    public Server(int serverPort) throws IOException {
+        this.serverPort = serverPort;
+        this.serverSocket = new ServerSocket(this.serverPort);
+        this.clientId = 0;
+        this.clients = new ArrayList<>();
+
+    }
+
+    public void StartServer() {
+
+        this.start();
+    }
+
+    public void RemoveClient(int id) {
+
+        SClient refClient = null;
+        for (SClient client : clients) {
+            if (client.id == id) {
+                refClient = client;
+                break;
+            }
+        }
+        if (refClient != null) {
+            this.clients.remove(refClient);
+        }
+        RefreshClientList();
+
+    }
+
+    
+
+    public void SendAll(String message) {
+        for (SClient client : clients) {
+            try {
+                client.clientOutput.writeObject(message);
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
+    }
+
+    public void SendToClient(SClient client, String message) {
+
+        try {
+            client.clientOutput.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+    }
+
+    public void SendToClient(int index, String message) {
+        try {
+            this.clients.get(index).clientOutput.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+                Socket client = serverSocket.accept(); //blocking
+                this.clientId++;
+                SClient newClient = new SClient(this, client, this.clientId);
+                this.clients.add(newClient);
+                newClient.Listen();
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void RemoveClient(SClient client) {
+        this.clients.remove(client);
+        RefreshClientList();
+    }
+
+    public void RefreshClientList() {
+        Frm_Server.lst_clientsModel.removeAllElements();
+        for (SClient client : clients) {
+            Frm_Server.lst_clientsModel.addElement("id: " + client.id
+                    + " ip: " + client.clientSocket.getInetAddress().toString()
+                    + "port: " + client.clientSocket.getPort());
+        }
+    }
+
+}
